8e1164fbed8a4a968f6d189bd7016cd96336c517
unknown <cassandrash@yandex.ru>
2021-05-16 15:49:22 +0300

    доработки для синхронизации с фронтом

--- config/corsoptions.js
+++ config/corsoptions.js
@@ -1,7 +1,7 @@
 const options = {
   origin: [
 
-    'https://cassandrash.nomoredomains.icu', 'http://cassandrash.nomoredomains.icu',
+    'https://cassandrash.nomoredomains.icu', 'http://cassandrash.nomoredomains.icu', 'http://localhost:5000',
   ],
   methods: ['GET', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
